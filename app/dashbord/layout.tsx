4b1398493f3a8076e05c8f20a8e7e1e762682326
Salama <122737355+SalamaHani@users.noreply.github.com>
2025-07-07 13:58:15 +0300

    push

--- app/dashbord/layout.tsx
+++ app/dashbord/layout.tsx
@@ -1,8 +1,24 @@
+import Continer from "@/components/global/Continer";
+import Providers from "../Providers";
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Astorefront | Dashbord | Admin",
+  description: "A nifty Astorefonte Shope Abagor Laump  built with Next.js",
+  keywords: ["Lumps", "online Lumps cotigers", "Table", "sout"],
+  authors: [{ name: "Astorefront" }],
+  icons: {
+    icon: ["/favicon.ico"],
+  },
+};
 function DashbordLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div className="py-20">{children}</div>
-    </>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Providers>
+          <Continer className="py-20">{children}</Continer>
+        </Providers>
+      </body>
+    </html>
   );
 }
 export default DashbordLayout;
