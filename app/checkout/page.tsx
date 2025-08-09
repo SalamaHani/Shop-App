9e7f33944a3026e205b94908cdc353c471fdc677
Salama <122737355+SalamaHani@users.noreply.github.com>
2025-08-09 17:18:04 +0300

    push

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,13 +2,11 @@ import TitelSection from "@/components/global/TitelSection";
 import { getUserFromSession } from "@/lib/auth";
 import { cookies } from "next/headers";
 import React from "react";
-import { redirect } from "next/navigation";
 import { fetchOrCreateCart, updateCart } from "@/utils/actions";
 import Checoutmaren from "@/components/chekout/Checoutmaren";
 
 async function page() {
   const user = await getUserFromSession(await cookies());
-  if (user == null) return redirect("/");
   const cart = await fetchOrCreateCart({ userID: user.id });
   const { currentCart } = await updateCart(cart);
 
