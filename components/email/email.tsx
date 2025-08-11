36a35d39aeeb272f4e3576d382a299b789bca442
Salama <122737355+SalamaHani@users.noreply.github.com>
2025-08-11 13:05:57 +0300

    ediet domins

--- components/email/email.tsx
+++ components/email/email.tsx
@@ -1,63 +1,3 @@
-// import {
-//   Body,
-//   Container,
-//   Head,
-//   Heading,
-//   Html,
-//   Preview,
-//   Text,
-// } from "@react-email/components";
-
-// interface WaitlistEmailProps {
-//   name: string;
-// }
-
-// export const WaitlistEmail: React.FC<Readonly<WaitlistEmailProps>> = ({
-//   name,
-// }) => (
-//   <Html>
-//     <Head />
-//     <Preview>Thank you for joining our waitlist and for your patience</Preview>
-//     <Body style={main}>
-//       <Container style={container}>
-//         <Heading style={h1}>Reset your password</Heading>
-//         <Text style={text}>
-//           Thank you {name} for joining our waitlist and for your patience. We
-//           will send you a note when we have something new to share.
-//         </Text>
-//       </Container>
-//     </Body>
-//   </Html>
-// );
-
-// export default WaitlistEmail;
-
-// const main = {
-//   backgroundColor: "#000000",
-//   margin: "0 auto",
-//   fontFamily:
-//     "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
-// };
-
-// const container = {
-//   margin: "auto",
-//   padding: "96px 20px 64px",
-// };
-
-// const h1 = {
-//   color: "#ffffff",
-//   fontSize: "24px",
-//   fontWeight: "600",
-//   lineHeight: "40px",
-//   margin: "0 0 20px",
-// };
-
-// const text = {
-//   color: "#aaaaaa",
-//   fontSize: "14px",
-//   lineHeight: "24px",
-//   margin: "0 0 40px",
-// };
 import {
   Body,
   Button,
@@ -84,7 +24,7 @@ export const DropboxResetPasswordEmail = ({
   email,
   resetPasswordLink,
 }: DropboxResetPasswordEmailProps) => {
-  const href = `https://astorefront-git-main-salamahanis-projects.vercel.app/resetpassword?token=${resetPasswordLink}&email=${email}`;
+  const href = `https://astorefront.vercel.app/resetpassword?token=${resetPasswordLink}&email=${email}`;
   ///link routs app
   return (
     <Html>
